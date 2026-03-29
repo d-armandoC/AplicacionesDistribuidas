75f8a7b3832b2f338a567714d7cd7efbb9379187
dcale <diego.cale92@gmail.com>
2026-03-28 20:44:07 -0500

    Unidad 2 clase 6

--- src/main/java/Clase6/SistemaEventos.java
+++ src/main/java/Clase6/SistemaEventos.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Clase6;
 
 import java.util.concurrent.ExecutorService;
